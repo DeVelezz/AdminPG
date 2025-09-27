5d18d5fb64832885b6ee0d0f5045afba91123b65
Jpvlz <jpvlz@ejemplo.com>
2025-09-27 17:48:44 -0500

    remove history

--- .history/AdminPG/frontend/src/components/SectionHeader_20250922185318.jsx
+++
@@ -1,7 +0,0 @@
-function SectionHeader({children}){
-    return(
-        <header className="w-full h-20 bg-white justify-between items-center flex p-4 relative z-20">
-            {children}
-        </header>
-    );
-}
